9144411e4b0e91b9d306f899408a0257d3ad67f1
Meff86 <meff86@list.ru>
2022-08-10 19:33:31 +0300

    добавил кастомные 404 и 500 страницы ошибок.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -65,7 +65,7 @@ elif os.environ.get('DEBUG') == 'FALSE':
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
