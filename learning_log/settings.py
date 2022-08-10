04c3d520f4752756e3c29f358d30d83b6d51e9c6
Meff86 <meff86@list.ru>
2022-08-10 16:18:49 +0300

    Установил DEBUG основаный на переменных средах

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,6 +56,11 @@ ROOT_URLCONF = 'learning_log.urls'
 LOGIN_URL = '/users/login/'
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
 
 TEMPLATES = [
     {
