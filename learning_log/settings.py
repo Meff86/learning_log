25ef81dda0640e5fd50a862143cd303bc26a0e90
Meff86 <110338403+Meff86@users.noreply.github.com>
2022-08-11 21:11:33 +0300

    Update settings.py
    убрал секретный ключ

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xl7+9ax$bnxa71pyt72sq=h=(m-(3mqj0v_$n)^yr52!_k3u7h'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
